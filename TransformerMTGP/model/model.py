c09a7024ade06f49a9da3d47f4e76ab79e68cd75
Mao <maoshanshi@Maos-Laptop.local>
2025-02-21 15:28:21 +1300

    reduce the threshold

--- TransformerMTGP/model/model.py
+++ TransformerMTGP/model/model.py
@@ -129,7 +129,7 @@ class MyNN(nn.Module):
             times += 1
             if times % 5 == 0:
                 print("The training loss value is:", training_loss_value.item())
-            if times > epoch_times and sum(training_loss[-5:]) / 5 > 1.8:
+            if times > epoch_times and sum(training_loss[-5:]) / 5 > 2:
                 epoch_times += 10
                 print(
                     f"The average loss value of latest 5 epochs is {sum(training_loss[-5:]) / 5}. Add 10 more epochs to {epoch_times}"

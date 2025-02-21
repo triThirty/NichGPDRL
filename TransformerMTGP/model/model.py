1b719afa1de38d9e3d57ec88cfacb5ccbb4966a7
Mao <maoshanshi@Maos-Laptop.local>
2025-02-22 12:51:17 +1300

    add more training epoch

--- TransformerMTGP/model/model.py
+++ TransformerMTGP/model/model.py
@@ -129,8 +129,8 @@ class MyNN(nn.Module):
             times += 1
             if times % 5 == 0:
                 print("The training loss value is:", training_loss_value.item())
-            if times > epoch_times and sum(training_loss[-5:]) / 5 > 2:
-                epoch_times += 10
+            if times > epoch_times and sum(training_loss[-5:]) / 5 > 1.2:
+                epoch_times += 20
                 print(
                     f"The average loss value of latest 5 epochs is {sum(training_loss[-5:]) / 5}. Add 10 more epochs to {epoch_times}"
                 )

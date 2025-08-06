451b89464f183c5d23c882c693c7855caed0da72
Mao <maoshanshi@Maos-Laptop-4.local>
2025-08-06 23:14:02 +1200

    more complext decision situations

--- MTGP_KNN/GPFC.py
+++ MTGP_KNN/GPFC.py
@@ -453,7 +453,7 @@ def GPFC_main(config):
         sequencing_rule=rule_S,
         seed=rd["seed"],
         ifPrint=False,
-        dataset_name=rd["dataset_name"],
+        dataset_name="LL",
     )
     spf.simulation()
 

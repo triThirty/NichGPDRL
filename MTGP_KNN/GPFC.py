50924fe8196a539db3a99a0cbf18fc5b74e980e9
Mao <maoshanshi@Maos-Laptop-4.local>
2025-08-06 23:22:07 +1200

    use LH to generate decision situations

--- MTGP_KNN/GPFC.py
+++ MTGP_KNN/GPFC.py
@@ -453,7 +453,7 @@ def GPFC_main(config):
         sequencing_rule=rule_S,
         seed=rd["seed"],
         ifPrint=False,
-        dataset_name="LL",
+        dataset_name="LH",
     )
     spf.simulation()
 
